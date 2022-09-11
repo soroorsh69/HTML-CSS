7cba84a7c5ce5615404583b4cfc5606c473af8c1
Soroor sheibani <seo.sheibani@gmail.com>
2022-09-11 18:00:56 +0430

    Add my sql code

--- index.php
+++ index.php
@@ -39,11 +39,13 @@ echo '<li class="nav-item"><a class="nav-link" href="#">'.$value.'</a></li>';
         <?php
         $content=file_get_contents("resume.txt");
         echo $content;
-
+        ?>
+        <br>
+<?php
         $db_host = '127.0.0.1';
         $db_user = 'root';
   $db_password = 'root';
-  $db_db = 'Soroor-db';
+  $db_db = 'Soroor_db';
   $db_port = 8889;
 
   $mysqli = new mysqli(
@@ -55,16 +57,20 @@ echo '<li class="nav-item"><a class="nav-link" href="#">'.$value.'</a></li>';
   );
 	
   if ($mysqli->connect_error) {
-    echo 'Errno: '.$mysqli->connect_errno;
-    echo '<br>';
-    echo 'Error: '.$mysqli->connect_error;
-    exit();
+    die("Connection failed: " . $mysqli->connect_error);
   }
 
-  echo 'Success: A proper connection to MySQL was made.';
-  echo '<br>';
-  echo 'Host information: '.$mysqli->host_info;
-  echo '<br>';
+  $sql = "SELECT id, firstname, lastname FROM MyGuests";
+  $result = $conn->query($sql);
+  
+  if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    }
+  } else {
+    echo "0 results";
+  }
   echo 'Protocol version: '.$mysqli->protocol_version;
 
   $mysqli->close();
